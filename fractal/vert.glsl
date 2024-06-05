605336e448817d61898b1bdbfb2e0b5aaed73405
Шаповалов Даниил <cgsg187@ad.pml30.ru>
2024-06-05 09:04:52 +0300

    Replaced some files.

--- fractal/vert.glsl
+++
@@ -1,7 +0,0 @@
-#version 300 es
-in highp vec4 in_pos;
-
-void main( void )
-{
-  gl_Position = in_pos;
-}
--- plathonBodies/src/mth.js
+++
@@ -1,7 +0,0 @@
-export function d2r(a) {
-  return a * (Math.PI / 180.0);
-}
-
-export function r2d(a) {
-  return a * (180.0 / Math.PI);
-}
